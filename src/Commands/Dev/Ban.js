198566ce2b3db3ce86632e96315ed160a806852f
Toshi_さん <91844428+Toshi-san001@users.noreply.github.com>
2023-04-17 14:14:04 +0530

    Update Ban.js

--- src/Commands/Dev/Ban.js
+++ src/Commands/Dev/Ban.js
@@ -21,58 +21,38 @@ module.exports = class command extends Command {
       let { context } = args[0]
       const users = M.mentioned
       if (quoted && !users.includes(quoted.sender.jid)) users.push(quoted.sender.jid)
-
       if (users.length <= 0) return void M.reply('Tag or quote a user to ban with the reason')
-
       const { ban } = await this.helper.DB.getUser(users[0])
-
       if (ban?.banned)
-
-          return void M.reply(
+      return void M.reply(
 
               `🟥 *@${users[0].split('@')[0]}* is already banned by *${ban.bannedBy}* in *${ban.bannedIn} ${
                   ban.time
               } (GMT)*. ❓ Reason: *${ban.reason}*`,
 
               'text',
-
               undefined,
-
               undefined,
-
               undefined,
-
               [users[0]]
 
           )
 
       if (!context)
-
           return void M.reply(`Provide the reason to ban. Example - *${this.helper.config.prefix}ban @user | reason*`)
-
       const reason = context.trim().split('|')[1]
-
-      if (!reason)
-
-          return void M.reply(`Provide the reason to ban. Example - *${this.helper.config.prefix}ban @user | reason*`)
-
-      const { subject } = 
-
-      await this.helper.DB.banUser(users[0], sender.username, subject, reason.trim())
-
-      return void M.reply(
-
-          `*@${users[0].split('@')[0]}* is now banned from using commands. Reason: *${reason.trim()}*`,
+    if (!reason)
+        return void M.reply(`Provide the reason to ban. Example - *${this.helper.config.prefix}ban @user | reason*`)
+   const { subject } = 
+  await this.helper.DB.banUser(users[0], sender.username, subject, reason.trim())
+   return void M.reply(
+     `*@${users[0].split('@')[0]}* is now banned from using commands. Reason: *${reason.trim()}*`,
 
           'text',
-
-          undefined,
-
-          undefined,
-
-          undefined,
-
-          [users[0]]
+       undefined,
+       undefined,
+       undefined,
+       [users[0]]
 
       )
 
