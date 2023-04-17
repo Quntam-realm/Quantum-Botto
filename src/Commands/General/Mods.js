133557edee00167787fcad9a7e69750d6ad1988c
Toshi_さん <91844428+Toshi-san001@users.noreply.github.com>
2023-04-17 18:39:03 +0530

    Update Mods.js

--- src/Commands/General/Mods.js
+++ src/Commands/General/Mods.js
@@ -1,6 +1,7 @@
 const Command = require('../../Structures/Command')
 
 module.exports = class command extends Command {
+
    constructor() {
    super('mods', {
    description: 'Shows bot owner',
@@ -13,23 +14,26 @@ module.exports = class command extends Command {
         })
    }
 
-  /**
-
+   /**
      * @param {Message} M
      * @returns {Promise<void>}
      */
 
      execute = async (M) => {
+
      if (!this.helper.config.mods.length) return void M.reply('*[UNMODERATED]*')
      let text ="*❱❱❱❱❱ MODS ❰❰❰❰❰*\n\n"
 
 const mentions = []
+
 this.helper.config.mods.map((x)=>{
 text+= `*❯ @${this.helper.contact.getContact(x).jid.split('@')[0]}*\n`
 mentions.push(this.helper.contact.getContact(x).jid)
 
 })
+
         return void this.client.sendMessage(M.from,{text:text, mentions:mentions})
 
     }
+
 }
