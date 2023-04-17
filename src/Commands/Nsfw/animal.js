a501dc3b4bba4b45f0c96586b5b16cadaf43842b
Toshi_さん <91844428+Toshi-san001@users.noreply.github.com>
2023-04-17 19:47:45 +0530

    Update animal.js

--- src/Commands/Nsfw/animal.js
+++ src/Commands/Nsfw/animal.js
@@ -1,40 +1,33 @@
 const Command = require('../../Structures/Command')
-const axios  = require('axios')
-const Message = require('../../Structures/Message')
 
 module.exports = class command extends Command {
-
     constructor() {
-
-        super('animal', {
-
-            description: 'Sends random animal image',
-
+        super('blowjob', {
+            description: 'Sends a random nsfw blowjob image',
             category: 'nsfw',
-
-            usage: 'animal',
-
+            usage: 'blowjob',
             exp: 20,
-
             cooldown: 5
-
         })
-
     }
 
     /**
-
      * @param {Message} M
-
      * @returns {Promise<void>}
-
      */
 
     execute = async (M) => {
-
-        let buff= await axios.get(`https://fantox-apis.vercel.app/animal`)
-
-    let imgURL = buff.data.url
-
+       const prefix = this.helper.config.prefix
+       const buttons = [
+        { buttonId: `${this.helper.config.prefix}blowjob`, buttonText: {displayText: 'NEXT ➡️'}, type: 1},
+     ]
+       const buttonMessage = {
+       image: { url: 'https://api.waifu.pics/nsfw/blowjob'},
+       caption: '*Here, you go!*',
+       footer: `Quntuam-botto`,
+       buttons: buttons,
+       headerType: 1
+     }
+       return void (await this.client.sendMessage(M.from, buttonMessage))
     }
 }
