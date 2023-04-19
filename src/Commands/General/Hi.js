f57bd74ac1ea3af01b8f98c00c11f7e7032a0474
Examinati <Aliwadood561@gmail.com>
2023-04-20 00:00:58 +0500

    hmmv2232323

--- src/Commands/General/Hi.js
+++ src/Commands/General/Hi.js
@@ -22,14 +22,14 @@ module.exports = class command extends Command {
         // return void m.reply(`Hello! ${m.sender.username}`)
         // send a buttons message!
         const hello =[
-            {buttonId: `${process.env.PREFIX}help`, buttonText: {displayText: 'Commands'}, types: 2},
-            {buttonId: `${process.env.PREFIX}profile`, buttonText: {displayText: 'Profile'}, types: 1},
+            {buttonId: `${this.helper.config.prefix}help`, buttonText: {displayText: 'Commands'}, types: 2},
+            {buttonId: `${this.helper.config.prefix}profile`, buttonText: {displayText: 'Profile'}, types: 1},
      
         ]
         let buttonMessageds = {
          image: {url:"https://telegra.ph/file/75368c6fe4abb9d0f2bb9.png"},
          caption: `Hello! ${m.sender.username}` ,
-         footer: `${process.env.NAME}`,
+         footer: `${this.helper.config.name}`,
          buttons: hello,
          headerType: 4
      }
